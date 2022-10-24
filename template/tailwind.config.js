ece31329d6d876a4bc0bc74cafc91f3dc8d0df93
秦嘉鹏 <759848795@qq.com>
2022-10-24 11:07:25 +0800

    add tailwindcss

---
+++ template/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
